c87f1b16d2063cc3da9dbccc0b5b2e58b1dbe95c
Artem Tsys <19successful91@gmail.com>
2021-02-21 21:23:06 +0200

    fix sentence in greeting

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ function greeting() {
   console.log('Welcome to the Brain Games!');
   console.log('May I have your name?');
   const name = readlineSync.question('Your answer: ');
-  console.log(`Hello! ${name}`);
+  console.log(`Hello, ${name}!`);
   return name;
 }
 
